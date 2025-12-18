724ee50e9146f4f005e573a08d69119570191a75
Anuj Sain <mailanujsain@yahoo.com>
2025-12-19 02:39:57 +0530

    minor fix

--- src/components/layouts/dashboard/admin-dashboard-sidebar.tsx
+++ src/components/layouts/dashboard/admin-dashboard-sidebar.tsx
@@ -115,10 +115,10 @@ export function AdminDashboardSidebar({
           <SidebarMenuItem>
             <SidebarMenuButton
               asChild
-              className="data-[slot=sidebar-menu-button]:!p-1.5"
+              className="data-[slot=sidebar-menu-button]:p-1.5!"
             >
               <a href="#">
-                <IconInnerShadowTop className="!size-5" />
+                <IconInnerShadowTop className="size-5!" />
                 <span className="text-base font-semibold">EBC Marketplace</span>
               </a>
             </SidebarMenuButton>
