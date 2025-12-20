2f972d0a0cf585939dec7b47836afaed814a0255
Rupam Kairi <rupamkairi@hotmail.com>
2025-12-21 03:37:00 +0530

    Create Admin Forms Dialogs not opening - DialogTrigger brought inside TooltipTrigger (ignore next issue.)

--- src/components/admin/create-admin-executive-form.tsx
+++ src/components/admin/create-admin-executive-form.tsx
@@ -59,16 +59,16 @@ export function CreateAdminExecutiveForm() {
 
   return (
     <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Tooltip>
-          <TooltipTrigger>
+      <Tooltip>
+        <TooltipTrigger>
+          <DialogTrigger asChild>
             <Button disabled={disabled}>Add Executive</Button>
-          </TooltipTrigger>
-          <TooltipContent hidden={!disabled}>
-            <p>Only Admin & Manager can add executive</p>
-          </TooltipContent>
-        </Tooltip>
-      </DialogTrigger>
+          </DialogTrigger>
+        </TooltipTrigger>
+        <TooltipContent hidden={!disabled}>
+          <p>Only Admin & Manager can add executive</p>
+        </TooltipContent>
+      </Tooltip>
       <DialogContent className="sm:max-w-[425px]">
         <DialogHeader>
           <DialogTitle>Add Admin Executive</DialogTitle>
