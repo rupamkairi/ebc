e17fcbc9dbfd4205980b31223705ffd431286dbf
Rupam Kairi <rupamkairi@hotmail.com>
2025-12-21 22:27:17 +0530

    prepare.

--- src/app/(dashboard)/seller-dashboard/page.tsx
+++ src/app/(dashboard)/seller-dashboard/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 export default function SellerDashboardPage() {
   return (
     <div>
---
+++ src/app/(public)/features/for-buyers/page.tsx
@@ -0,0 +1,7 @@
+export default function FeaturesForBuyersPage() {
+  return (
+    <div>
+      <h1>Features For Buyers</h1>
+    </div>
+  );
+}
