8c677571fd2ddea5614c60135dcffc9012aeb29a
Anuj Sain <mailanujsain@yahoo.com>
2025-12-24 22:40:48 +0530

    add indian currency image
    On the home page, initially there was an image of dollar bills which has been changed to indian currency.

--- src/components/layouts/landing-scrap/problem-section.tsx
+++ src/components/layouts/landing-scrap/problem-section.tsx
@@ -10,7 +10,7 @@ export function ProblemSection() {
     {
       title: t("problem_1_desc"),
       image:
-        "https://images.unsplash.com/photo-1553729459-efe14ef6055d?q=80&w=800",
+        "https://images.unsplash.com/photo-1729077555348-4ab768b4a90e?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
       icon: "🤔",
     },
     {
