582317adc5b0789e351cec0388da59f8b75346cc
Anuj Sain <mailanujsain@yahoo.com>
2026-02-20 03:04:25 +0530

    removed padding
    It's not much. See the changes.

--- src/app/browse/[id]/layout.tsx
+++ src/app/browse/[id]/layout.tsx
@@ -3,5 +3,5 @@ export default function ItemDetailsLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="container mx-auto py-6 px-4 md:px-6">{children}</div>;
+  return <div>{children}</div>;
 }
