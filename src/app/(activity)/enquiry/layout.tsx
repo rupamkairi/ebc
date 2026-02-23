c2495dc4f1c39694b828c26c6c2b361b8e4bdfc6
Anuj Sain <mailanujsain@yahoo.com>
2026-02-23 19:11:38 +0530

    ...

--- src/app/(activity)/enquiry/layout.tsx
+++ src/app/(activity)/enquiry/layout.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { BuyerDashboardSidebar } from "@/components/layouts/dashboard/buyer-dashboard-sidebar";
+import BuyerDashboardHeader from "@/components/layouts/dashboard/buyer-dashboard-header";
+import LayoutProvider from "@/components/layouts/dashboard/layout-provider";
+
 export default function EnquiryLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <LayoutProvider>
+      <div className="flex min-h-screen flex-col bg-[#F8F9FC] w-full">
+        <BuyerDashboardHeader />
+        <div className="flex flex-1 w-full flex-row">
+          <BuyerDashboardSidebar />
+          <main className="flex-1 overflow-y-auto w-full">
+            {children}
+          </main>
+        </div>
+      </div>
+    </LayoutProvider>
+  );
 }
